02a681e6211593612b219a2c730eb9e4ab7062c9
Nitesh <nnehrwal2002@gmail.com>
2025-11-25 14:33:06 +0530

    Add user service and related configurations for user validation

---
+++ gateway/src/main/java/com/fitness/gateway/user/UserService.java
@@ -0,0 +1,32 @@
+package com.fitness.gateway.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final WebClient userServiceWebClient;
+
+
+    public boolean validateUser(String userId) {
+        log.info("Calling user service for {}", userId);
+       try {
+            Boolean result = userServiceWebClient.get()
+                   .uri("/api/users/{userId}/validate", userId)
+                   .retrieve()
+                   .bodyToMono(Boolean.class)
+                   .block();
+            return Boolean.TRUE.equals(result);
+
+       }catch (WebClientResponseException e){
+           e.printStackTrace();
+       }
+        return false;
+    }
+}
